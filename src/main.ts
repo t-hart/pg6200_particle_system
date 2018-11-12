a6fbe776da24815fdd1cac5d4bd241f3508f514f
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2018-11-12 08:12:59 +0100

    Adds material and texture to male

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import * as three from 'three'
 import { OrbitControls } from './orbitControls'
-import { OBJLoader } from 'three-obj-mtl-loader'
+import { OBJLoader, MTLLoader } from 'three-obj-mtl-loader'
 
 //models
 import male from 'assets/obj/male02/male02.obj'
+import maleMtl from 'assets/obj/male02/male02.mtl'
+import uvTexture from 'assets/textures/UV_Grid_Sm.jpg'
 
 const initControls = (renderer: three.WebGLRenderer) => (camera: three.PerspectiveCamera) => {
   const controls = new OrbitControls(camera, renderer.domElement)
@@ -35,7 +37,7 @@ const initCamera = (width: number, height: number) => {
 }
 
 const initDirectionalLight = () => {
-  const directional = new three.DirectionalLight(0xffffff, .5)
+  const directional = new three.DirectionalLight(0xffffff, .8)
   directional.position.set(10, 10, 10)
   directional.castShadow = true
   directional.shadow.mapSize.width = 1024
@@ -43,6 +45,8 @@ const initDirectionalLight = () => {
 
   return directional
 }
+
+const initAmbientLight = () => new three.AmbientLight(0xcccccc, 0.2)
 
 const initScene = () => new three.Scene()
 
@@ -52,15 +56,34 @@ export const init = (width: number, height: number) => (canvas: HTMLCanvasElemen
   const renderer = initRenderer(width, height)(canvas)
   const camera = initCamera(width, height)
   const directionalLight = initDirectionalLight()
+  const ambientLight = initAmbientLight()
 
+  scene.add(ambientLight)
   scene.add(directionalLight)
   initControls(renderer)(camera)
 
   const objLoader = new OBJLoader()
-  objLoader.load(male, function(obj) { scene.add(obj) },
-    xhr => console.log(`${xhr.loaded / xhr.total * 100}% loaded`),
-    e => console.log('Encountered an error while loading the model:', e)
-  )
+  const mtlLoader = new MTLLoader()
+  const textureLoader = new three.TextureLoader()
+
+  const texture = textureLoader.load(uvTexture)
+
+  mtlLoader.load(maleMtl, (materials) => {
+    materials.preload()
+    objLoader.setMaterials(materials)
+    // objLoader.load(male, function(obj) {
+    objLoader.load(male, obj => {
+      obj.traverse(child => {
+        if (child.isMesh) {
+          child.material.map = texture
+        }
+      })
+      scene.add(obj)
+    },
+      xhr => console.log(`${xhr.loaded / xhr.total * 100}% loaded`),
+      e => console.log('Encountered an error while loading the model:', e)
+    )
+  })
 
   const animate = () => {
     requestAnimationFrame(animate)
